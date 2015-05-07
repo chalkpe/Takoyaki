aa3f88f469d6640adedf1896273ddc1c3c287c75
ChalkPE <amato0617@gmail.com>
2015-05-07 18:19:26 +0900

    Optimization for reading properties And also added the option for excluded plugins

--- src/main/java/pe/chalk/takoyaki/Takoyaki.java
+++ src/main/java/pe/chalk/takoyaki/Takoyaki.java
@@ -24,14 +24,16 @@ import pe.chalk.takoyaki.logger.Prefix;
 import pe.chalk.takoyaki.logger.ConsoleLogger;
 import pe.chalk.takoyaki.plugin.Plugin;
 
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -43,34 +45,49 @@ public class Takoyaki implements Prefix {
     public static final String VERSION = "2.0";
 
     private static Takoyaki instance = null;
+    private static List<String> DEFAULT_CONFIG = Arrays.asList(
+            "{",
+            "  \"targets\": [",
+            "    {",
+            "      \"address\": \"minecraftpe\",",
+            "      \"prefix\": \"M.K\",",
+            "      \"interval\": 2500,",
+            "      \"timeout\": 1000,",
+            "      \"filters\": [\"article\", \"commentary\", \"visitation\"]",
+            "    }",
+            "  ],",
+            "  \"options\": {",
+            "    \"output\": \"Takoyaki.log\",",
+            "    \"excluded\": [\"debug.js\"]",
+            "  }",
+            "}");
 
     private List<Target> targets;
     private List<Plugin> plugins;
     private ConsoleLogger logger;
 
-    private boolean isAlive;
+    private boolean isAlive = false;
 
     public static Takoyaki getInstance(){
         return instance;
     }
 
-    public Takoyaki() throws JSONException, IOException{
+    public Takoyaki() throws JSONException, IOException {
         Takoyaki.instance = this;
-        this.logger = new ConsoleLogger(System.out);
 
-        JSONObject properties;
-        try{
-            properties = new JSONObject(Files.lines(Paths.get("properties.json"), Charset.forName("UTF-8")).collect(Collectors.joining()));
-        }catch(IOException e){
-            this.getLogger().error("properties.json 파일을 읽을 수 없습니다 : " + e.getMessage());
-
-            System.exit(1);
-            return;
+        Path propertiesPath = Paths.get("properties.json");
+        if(!Files.exists(propertiesPath)){
+            Files.write(propertiesPath, DEFAULT_CONFIG, Charset.forName("UTF-8"), StandardOpenOption.CREATE_NEW, StandardOpenOption.WRITE);
         }
+        JSONObject properties = new JSONObject(Files.lines(propertiesPath, Charset.forName("UTF-8")).collect(Collectors.joining()));
 
-        if(properties.has("options")){
-            JSONObject optionsObject = properties.getJSONObject("options");
-            this.getLogger().out = new PrintStream(new FileOutputStream(optionsObject.getString("output"), true));
+        JSONObject optionsObject = properties.getJSONObject("options");
+        this.logger = new ConsoleLogger(new PrintStream(new FileOutputStream(optionsObject.getString("output"), true)));
+
+        JSONArray excludedArray = optionsObject.getJSONArray("excluded");
+        ArrayList<String> excluded = new ArrayList<>();
+        for(int i = 0; i < excludedArray.length(); i++){
+            excluded.add(excludedArray.getString(i));
         }
 
         JSONArray targetsArray = properties.getJSONArray("targets");
@@ -79,31 +96,28 @@ public class Takoyaki implements Prefix {
             this.targets.add(new Target(this, targetsArray.getJSONObject(i)));
         }
 
-        File pluginDirectory = new File("plugins");
-        if(!pluginDirectory.exists()){
-            if(pluginDirectory.mkdir()){
-                this.getLogger().debug("plugin 디렉토리를 생성했습니다");
-            }
+        Path pluginsPath = Paths.get("plugins");
+        if(Files.exists(pluginsPath)){
+            Files.createDirectories(pluginsPath);
+            this.getLogger().debug("plugin 디렉토리를 생성했습니다");
         }
 
-        File[] pluginFiles = pluginDirectory.listFiles(file -> file.getName().endsWith(".js"));
-        if(pluginFiles != null){
-            this.plugins = new ArrayList<>(pluginFiles.length);
-            for(File pluginFile : pluginFiles){
-                try{
-                    Plugin plugin = new Plugin(pluginFile);
+        this.plugins = new ArrayList<>();
+        Files.list(pluginsPath).filter(path -> path.endsWith(".js") && !excluded.contains(path.getFileName().toString())).map(Path::toFile).forEach(pluginFile -> {
+            try{
+                Plugin plugin = new Plugin(pluginFile);
 
-                    this.getLogger().debug("플러그인 " + plugin.getName() + "을(를) 불러왔습니다");
-                    this.plugins.add(plugin);
-                }catch(JavaScriptException | IOException e){
-                    this.getLogger().error(e.getMessage());
-                }
+                this.getLogger().debug("플러그인 " + plugin.getName() + "을(를) 불러왔습니다");
+                this.plugins.add(plugin);
+            }catch(JavaScriptException | IOException e){
+                this.getLogger().error(e.getMessage());
             }
-            this.plugins.forEach(plugin -> plugin.call("onCreate", new Object[]{plugin.getName()}));
-            this.getLogger().newLine();
-        }
+        });
 
-        Runtime.getRuntime().addShutdownHook(new Thread(){
+        this.plugins.forEach(plugin -> plugin.call("onCreate", new Object[]{plugin.getName()}));
+        this.getLogger().newLine();
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
             public void run(){
                 Takoyaki.this.getLogger().newLine();
@@ -113,7 +127,8 @@ public class Takoyaki implements Prefix {
                 Takoyaki.this.getPlugins().forEach(plugin -> plugin.call("onDistroy", Context.emptyArgs));
             }
         });
-        this.isAlive = false;
+
+        Files.write(propertiesPath, properties.toString().getBytes("UTF-8"), StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
     }
 
     public List<Target> getTargets(){
