c5cfc2ad09a904d746fced11ca90426cdc11cf70
파차리로디드 <fcreloaded@outlook.kr>
2015-05-30 12:56:56 +0900

    Start Working on basic bukkit event support
    Start Working on basic bukkit event support

---
+++ Takoyaki-Bukkit/src/main/java/pe/chalk/takoyaki_bukkit/TakoyakiEvent.java
@@ -0,0 +1,23 @@
+package pe.chalk.takoyaki_bukkit;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class TakoyakiEvent extends Event {
+
+	private static final HandlerList handlers = new HandlerList();
+	private String message;
+	
+	TakoyakiEvent(String message) {
+		this.message = message;
+	}
+	
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
