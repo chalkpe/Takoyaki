64870a69303ca8d77b5401f3329a2d962f8f35a0
ChalkPE <chalkpe@gmail.com>
2015-10-31 23:35:33 +0900

    Version bump

--- Takoyaki/src/main/java/pe/chalk/takoyaki/Takoyaki.java
+++ Takoyaki/src/main/java/pe/chalk/takoyaki/Takoyaki.java
@@ -45,7 +45,7 @@ import java.util.stream.Stream;
  * @since 2015-04-07
  */
 public class Takoyaki implements Prefix {
-    public static final String VERSION = "2.2.2-NAMU-0.1";
+    public static final String VERSION = "2.2.2-NAMU-0.1-SNAPSHOT";
 
     private static Takoyaki instance = null;
     private static final List<String> DEFAULT_CONFIG = Arrays.asList(
