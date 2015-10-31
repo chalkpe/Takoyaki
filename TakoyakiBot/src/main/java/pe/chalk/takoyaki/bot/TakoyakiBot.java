ad4248b12df932e02c1c9c3c7ede38cb6c46cb12
ChalkPE <chalkpe@gmail.com>
2015-10-31 22:17:16 +0900

    Code cleanup

--- TakoyakiBot/src/main/java/pe/chalk/takoyaki/bot/TakoyakiBot.java
+++ TakoyakiBot/src/main/java/pe/chalk/takoyaki/bot/TakoyakiBot.java
@@ -8,6 +8,7 @@ import de.vivistra.telegrambot.receiver.Receiver;
 import de.vivistra.telegrambot.sender.Sender;
 import de.vivistra.telegrambot.settings.BotSettings;
 import pe.chalk.takoyaki.Takoyaki;
+import pe.chalk.takoyaki.logger.Loggable;
 import pe.chalk.takoyaki.utils.TextFormat;
 
 import java.io.IOException;
@@ -15,18 +16,18 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * @author ChalkPE <amato0617@gmail.com>
+ * @author ChalkPE <chalkpe@gmail.com>
  * @since 2015-10-02
  */
 public class TakoyakiBot implements IReceiverService {
-    private Set<Integer> recipients = new HashSet<>();
+    private final Set<Integer> recipients = new HashSet<>();
 
-    public TakoyakiBot(String token) throws IOException {
+    public TakoyakiBot(final String token) throws IOException {
         BotSettings.setApiToken(token);
         Receiver.subscribe(this);
 
         Takoyaki takoyaki = new Takoyaki();
-        takoyaki.getLogger().addTransmitter((level, message) -> recipients.forEach(id -> Sender.send(new TextMessage(id, TextFormat.decode(message, TextFormat.Type.NONE)))));
+        takoyaki.getLogger().addTransmitter((level, message) -> this.recipients.forEach(id -> Sender.send(new TextMessage(id, TextFormat.decode(message, TextFormat.Type.NONE)))));
         takoyaki.start();
     }
 
@@ -38,32 +39,23 @@ public class TakoyakiBot implements IReceiverService {
         new TakoyakiBot(args[0]);
     }
 
-    public void received(Message message){
-        if(message.getMessageType() != MessageType.TEXT_MESSAGE){
-            return;
-        }
+    public void received(final Message message){
+        if(message.getMessageType() != MessageType.TEXT_MESSAGE) return;
 
-        String command = ((TextMessage) message).getMessage();
-        if(!command.startsWith("/")){
-            return;
-        }
+        final String command = ((TextMessage) message).getMessage();
+        if(!command.startsWith("/")) return;
 
-        String[] commands = command.substring(1).split(" ");
-        if(!commands[0].equalsIgnoreCase("takoyaki") && !commands[0].equalsIgnoreCase("takoyaki@takoyakibot")){
-            return;
-        }
+        final String[] commands = command.substring(1).split(" ");
+        if(!(commands[0].equalsIgnoreCase("takoyaki") || commands[0].equalsIgnoreCase("takoyaki@takoyakibot")) || commands.length < 2) return;
 
-        if(commands.length < 2){
-            return;
-        }
-
-        int target = message.isFromGroupChat() ? message.getGroupChat().getId() : message.getSender().getId();
+        final int target = message.isFromGroupChat() ? message.getGroupChat().getId() : message.getSender().getId();
         switch(commands[1].toLowerCase()){
             case "start":
                 this.recipients.add(target);
                 break;
 
             case "stop":
+            case "shutdown":
                 this.recipients.remove(target);
                 break;
 
@@ -71,6 +63,6 @@ public class TakoyakiBot implements IReceiverService {
                 return;
         }
 
-        System.out.println(TextFormat.RED.getAnsiCode() + this.recipients.toString() + TextFormat.RESET.getAnsiCode());
+        System.out.println(Loggable.Level.DEBUG.getFormats() + this.recipients.toString() + TextFormat.RESET.getAnsiCode());
     }
 }
